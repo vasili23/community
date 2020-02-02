d3c6502af56c6ffcd19289620372c7134757aa0d
wangrui <1123630646@qq.com>
2020-02-02 10:22:00 +0800

    notice fix messageinterceptor

---
+++ src/main/java/work/flipped/community/controller/interceptor/MessageInterceptor.java
@@ -0,0 +1,32 @@
+package work.flipped.community.controller.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import work.flipped.community.entity.User;
+import work.flipped.community.service.MessageService;
+import work.flipped.community.util.HostHolder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class MessageInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private MessageService messageService;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        User user = hostHolder.getUser();
+        if (user != null && modelAndView != null) {
+            int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
+            int noticeUnreadCount = messageService.findNoticeUnreadCount(user.getId(), null);
+            modelAndView.addObject("allUnreadCount", letterUnreadCount + noticeUnreadCount);
+        }
+    }
+}
