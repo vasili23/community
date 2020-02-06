a0d9b8fa0b96efc18d0b472c007fcfaa8f59955c
wangrui <1123630646@qq.com>
2020-02-06 23:08:58 +0800

    访问量统计

--- src/main/java/work/flipped/community/config/SecurityConfig.java
+++ src/main/java/work/flipped/community/config/SecurityConfig.java
@@ -49,7 +49,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements Comm
                         AUTHORITY_MODERATOR
                 )
                 .antMatchers(
-                        "/discuss/delete"
+                        "/discuss/delete",
+                        "/data/**"
                 )
                 .hasAnyAuthority(
                         AUTHORITY_ADMIN
--- src/main/java/work/flipped/community/config/WebMvcConfig.java
+++ src/main/java/work/flipped/community/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package work.flipped.community.config;
 
+import work.flipped.community.controller.interceptor.DataInterceptor;
 import work.flipped.community.controller.interceptor.LoginRequiredInterceptor;
 import work.flipped.community.controller.interceptor.LoginTicketInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private MessageInterceptor messageInterceptor;
 
+    @Autowired
+    private DataInterceptor dataInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
@@ -28,9 +32,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         // 使用Spring Security替代
         //registry.addInterceptor(loginRequiredInterceptor)
-         //       .excludePathPatterns("/**/*.css ", "/**/ *.js ", "/**/ *.png ", "/**/ *.jpg ", "/**/ *.jpeg ");
+        //       .excludePathPatterns("/**/*.css ", "/**/ *.js ", "/**/ *.png ", "/**/ *.jpg ", "/**/ *.jpeg ");
         registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(dataInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
     }
 
 }
